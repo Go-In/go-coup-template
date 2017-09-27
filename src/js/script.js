8c8e3ff299a11e99de610719d73f36d197a50987
komcal <kom.sujautra@gmail.com>
2017-09-27 22:00:29 +0700

    cart

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ const addItemToCart = (itemId) => {
   currentItems.push(itemId.toString());
   localStorage.setItem('gocoup-cart', [...new Set(currentItems)]);
 }
+
+const goToCart = () => {
+  const data = localStorage.getItem('gocoup-cart') || '';
+  const currentItems = data.split(',').filter(d => d !== '');
+  console.log(currentItems);
+  document.location = `cart.html?cart=${currentItems}`;
+}
